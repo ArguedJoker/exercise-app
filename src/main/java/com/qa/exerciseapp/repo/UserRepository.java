22999edab8b949051deedaf68e7500ae6d44d700
ArguedJoker <rohanpschauhan@gmail.com>
2020-08-30 20:53:52 +0100

    created UserRepository - initial commit

---
+++ src/main/java/com/qa/exerciseapp/repo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.qa.exerciseapp.repo;
+
+import com.qa.exerciseapp.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
