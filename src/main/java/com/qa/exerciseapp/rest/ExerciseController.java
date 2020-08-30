976deb960ea0c25389dcb88d37764bd7cf89d7cf
ArguedJoker <rohanpschauhan@gmail.com>
2020-08-30 17:06:23 +0100

    added getExerciseById endpoint

--- src/main/java/com/qa/exerciseapp/rest/ExerciseController.java
+++ src/main/java/com/qa/exerciseapp/rest/ExerciseController.java
@@ -3,8 +3,7 @@ package com.qa.exerciseapp.rest;
 import com.qa.exerciseapp.domain.ExerciseInfo;
 import com.qa.exerciseapp.service.ExerciseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,9 +22,18 @@ public class ExerciseController {
         return this.exerciseService.readAllActivities();
     }
 
-    /*@PostMapping("/createActivity")
-    public ExerciseInfo createActivity(ExerciseInfo exerciseInfo) {
-        return this.pumpitService.createActivity()
+    @PostMapping("/createActivities")
+    public ExerciseInfo createActivity(@RequestBody ExerciseInfo exerciseInfo) {
+        return this.exerciseService.createActivity(exerciseInfo);
     }
-*/
+
+    @DeleteMapping("/deleteActivity/{id}")
+    public Boolean deleteActivity(@PathVariable Long id) {
+        return this.exerciseService.deleteActivityById(id);
+    }
+
+    @GetMapping("/getActivityById/{id}")
+    public ExerciseInfo getExerciseById(@PathVariable Long id) {
+        return this.exerciseService.findActivityById(id);
+    }
 }
