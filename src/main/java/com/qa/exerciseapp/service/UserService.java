0d57019d45735c3e5c4725dd0d9cdd7513ab9c8c
ArguedJoker <rohanpschauhan@gmail.com>
2020-08-31 12:58:27 +0100

    created UserService - initial commit

--- src/main/java/com/qa/exerciseapp/service/UserService.java
+++ src/main/java/com/qa/exerciseapp/service/UserService.java
@@ -1,7 +1,41 @@
 package com.qa.exerciseapp.service;
 
+import com.qa.exerciseapp.domain.User;
+import com.qa.exerciseapp.exceptions.UserNotFoundException;
+import com.qa.exerciseapp.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository repo) { this.userRepository = repo; }
+
+    public List<User> readAllUsers() {
+        return this.userRepository.readAllUsers();
+    }
+
+    public User createUser(User user) {
+        return this.userRepository.save(user);
+    }
+
+    public User findUserById(Long id) {
+        return this.userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+    }
+
+    public User updateUser(Long id, User user) {
+        User update = findUserById(id);
+        update.setFirstName(user.getFirstName());
+        update.setLastName(user.getLastName());
+        update.setEmail(user.getEmail());
+        update.setPassword(user.getPassword());
+        update.setHeight(user.getHeight());
+        update.setWeight(user.getWeight());
+        return this.userRepository.save(update);
+    }
 }
