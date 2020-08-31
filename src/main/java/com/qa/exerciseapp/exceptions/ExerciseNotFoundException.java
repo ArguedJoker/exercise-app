bfcf28ee596d533dd051b100eb09421e01c01675
ArguedJoker <rohanpschauhan@gmail.com>
2020-08-31 12:51:18 +0100

    - renamed ActivityNotFoundException to ExerciseNotFoundException

--- src/main/java/com/qa/exerciseapp/exceptions/ExerciseNotFoundException.java
+++ src/main/java/com/qa/exerciseapp/exceptions/ExerciseNotFoundException.java
@@ -7,5 +7,5 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import javax.persistence.EntityNotFoundException;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "The Activity you are looking for does not exist")
-public class ActivityNotFoundException extends EntityNotFoundException {
+public class ExerciseNotFoundException extends EntityNotFoundException {
 }
