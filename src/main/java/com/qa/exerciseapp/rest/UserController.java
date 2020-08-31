a48e1f59d33de558fc9a062c4b166f475c01eefa
ArguedJoker <rohanpschauhan@gmail.com>
2020-08-31 18:04:36 +0100

    - created UserController - create User feature - delete User feature - find User feature - update User feature

--- src/main/java/com/qa/exerciseapp/rest/UserController.java
+++ src/main/java/com/qa/exerciseapp/rest/UserController.java
@@ -1,7 +1,37 @@
 package com.qa.exerciseapp.rest;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.qa.exerciseapp.domain.User;
+import com.qa.exerciseapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) { this.userService = userService; }
+
+    @GetMapping("/Users")
+    public List<User> getAllUsers() { return this.userService.readAllUsers(); }
+
+    @PostMapping("/createUser")
+    public User createUser(@RequestBody User user) { return this.userService.createUser(user); }
+
+    @DeleteMapping("/deleteUser/{id}")
+    public Boolean deleteUser(@PathVariable Long id) { return this.userService.deleteUserById(id); }
+
+    @GetMapping("/getUserById/{id}")
+    public User getUserById(@PathVariable Long id) { return this.userService.findUserById(id); }
+
+    @PutMapping("/updateUser/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return this.userService.updateUser(id, user);
+    }
+
+
+
 }
