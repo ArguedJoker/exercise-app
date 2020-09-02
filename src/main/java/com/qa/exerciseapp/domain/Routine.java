69650cff20510ad6f5b090d0ccb300f3eb8dff82
ArguedJoker <rohanpschauhan@gmail.com>
2020-09-02 15:20:33 +0100

    - updated Routine to get rid of uneccessary columns

--- src/main/java/com/qa/exerciseapp/domain/Routine.java
+++ src/main/java/com/qa/exerciseapp/domain/Routine.java
@@ -14,12 +14,6 @@ public class Routine {
     @Column
     private String routineName;
 
-    @Column
-    private Long userId;
-
-    @Column
-    private Long exerciseInfoId;
-
     public Long getRoutineId() {
         return routineId;
     }
@@ -35,8 +29,6 @@ public Routine() {}
     public Routine(String routineName, Long userId, Long exerciseInfoId) {
 
         this.routineName = routineName;
-        this.userId = userId;
-        this.exerciseInfoId = exerciseInfoId;
     }
 
     public void setRoutineId(Long routineId) {
@@ -49,26 +41,6 @@ public Routine() {}
 
     public void setRoutineName(String routineName) {
         this.routineName = routineName;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public Long getExerciseInfoId() {
-        return exerciseInfoId;
-    }
-
-    public void setExerciseInfoId(Long exerciseInfoId) {
-        this.exerciseInfoId = exerciseInfoId;
-    }
-
-    public List<ExerciseInfo> getExerciseInfo() {
-        return exerciseInfo;
     }
 
     public void setExerciseInfo(List<ExerciseInfo> exerciseInfo) {
