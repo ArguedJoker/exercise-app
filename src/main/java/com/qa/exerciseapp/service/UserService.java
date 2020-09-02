2682f31ae88cc646a42bdef510758cfaa1d0d1ca
ArguedJoker <rohanpschauhan@gmail.com>
2020-09-02 15:51:13 +0100

    - updated UserService to return DTOs

--- src/main/java/com/qa/exerciseapp/service/UserService.java
+++ src/main/java/com/qa/exerciseapp/service/UserService.java
@@ -1,42 +1,53 @@
 package com.qa.exerciseapp.service;
 
 import com.qa.exerciseapp.domain.User;
+import com.qa.exerciseapp.dto.UserDTO;
 import com.qa.exerciseapp.exceptions.UserNotFoundException;
 import com.qa.exerciseapp.repo.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final ModelMapper mapper;
 
     @Autowired
-    public UserService(UserRepository repo) { this.userRepository = repo; }
-
-    public List<User> readAllUsers() {
-        return this.userRepository.findAll();
+    public UserService(UserRepository repo, ModelMapper mapper) {
+        this.userRepository = repo;
+        this.mapper = mapper;
     }
 
-    public User createUser(User user) {
-        return this.userRepository.save(user);
+    private UserDTO mapToDTO(User user) {
+        return this.mapper.map(user, UserDTO.class);
     }
 
-    public User findUserById(Long id) {
-        return this.userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+    public List<UserDTO> readAllUsers() {
+        return this.userRepository.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    public User updateUser(Long id, User user) {
-        User update = findUserById(id);
+    public UserDTO createUser(User user) {
+        return this.mapToDTO(this.userRepository.save(user));
+    }
+
+    public UserDTO findUserById(Long id) {
+        return this.mapToDTO(this.userRepository.findById(id).orElseThrow(UserNotFoundException::new));
+    }
+
+    public UserDTO updateUser(Long id, User user) {
+        User update = this.userRepository.findById(id).orElseThrow(UserNotFoundException::new);
         update.setFirstName(user.getFirstName());
         update.setLastName(user.getLastName());
         update.setEmail(user.getEmail());
         update.setPassword(user.getPassword());
         update.setHeight(user.getHeight());
         update.setWeight(user.getWeight());
-        return this.userRepository.save(update);
+        return this.mapToDTO(this.userRepository.save(update));
     }
 
     public boolean deleteUserById(Long id) {
